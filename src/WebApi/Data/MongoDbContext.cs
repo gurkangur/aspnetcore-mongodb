5b937266d8aff26271e1f4804c66e3b965c5359c
gurkangur <gurkangur154@gmail.com>
2021-04-13 16:05:57 +0300

    add db context

---
+++ src/WebApi/Data/MongoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Linq;
+using WebApi.Domain.Common;
+using WebApi.Domain.Documents;
+
+namespace WebApi.Data
+{
+    public class MongoDbContext : IMongoDbContext
+    {
+        public IMongoDatabase _db;
+        public MongoDbContext()
+        {
+            var mongoUrl = new MongoUrl("mongodb://localhost/records");
+            var dbname = mongoUrl.DatabaseName;
+            _db = new MongoClient(mongoUrl).GetDatabase(dbname);
+        }
+
+        public IMongoCollection<Product> Products => _db.GetCollection<Product>(GetCollectionName(typeof(Product)));
+
+
+
+        public string GetCollectionName(Type documentType)
+        {
+            return ((BsonCollectionAttribute)documentType.GetCustomAttributes(
+                    typeof(BsonCollectionAttribute),
+                    true)
+                .FirstOrDefault())?.CollectionName;
+        }
+
+    }
+}
