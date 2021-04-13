139372208852d8e4f40b2a707639ae14a03ada70
gurkangur <gurkangur154@gmail.com>
2021-04-13 16:12:15 +0300

    add products controller

---
+++ src/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi.Domain.Documents;
+using WebApi.Interfaces;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpGet("id")]
+        public async Task<ActionResult<Product>> CreateProduct(string id)
+        {
+            var product = await _productRepository.GetAsync(id);
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+            await _productRepository.CreateAsync(product);
+            return NoContent();
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeleteProduct(string id)
+        {
+            await _productRepository.DeleteAsync(id);
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateProduct(string id, Product updatedProduct)
+        {
+            var queriedProduct = await _productRepository.GetAsync(id);
+            if (queriedProduct == null)
+            {
+                return NotFound();
+            }
+            await _productRepository.UpdateAsync(id, updatedProduct);
+            return NoContent();
+        }
+    }
+}
--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -22,7 +22,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IMongoDbContext, MongoDbContext>();
+            services.AddTransient<MongoDbContext>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
